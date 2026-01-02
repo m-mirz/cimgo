7464fc676b79a8aef7a3a04bfebc8a6335f0ae96
Markus Mirz <16180422+m-mirz@users.noreply.github.com>
2026-01-02 23:27:39 +0100

    add encoding package
    Signed-off-by: Markus Mirz <16180422+m-mirz@users.noreply.github.com>

--- encoding/cimgostructs/cim_constants.go
+++ encoding/cimgostructs/cim_constants.go
@@ -3,7 +3,13 @@
 package cimgostructs
 
 // CIM version string.
-const CIM_VERSION = "{{.CGMESVersion}}"
+const CIM_VERSION = "3.0.0";
 
 // Default namespaces used by CGMES. Map of namespace key to URL.
-var CIMNamespaces = map[string]string{}
+var CIMNamespaces = map[string]string{
+    "cim": "http://iec.ch/TC57/CIM100#",
+    "dm": "http://iec.ch/TC57/61970-552/DifferenceModel/1#",
+    "eu": "http://iec.ch/TC57/CIM100-European#",
+    "md": "http://iec.ch/TC57/61970-552/ModelDescription/1#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+}
